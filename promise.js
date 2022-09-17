4e0551d18e9bf57fe9b52e3416a3f24685aa0336
BakaKami <fransiskuscalvin1810@gmail.com>
2022-09-17 17:29:28 +0700

    create promiseOutput function on promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let count = 0;
+
+    const theaterIXXResult = await promiseTheaterIXX();
+    const theaterVGCResult = await promiseTheaterVGC();
+
+    theaterIXXResult.forEach((element) => {
+      if (element.hasil === emosi) {
+        count++;
+      }
+    });
+
+    theaterVGCResult.forEach((element) => {
+      if (element.hasil === emosi) {
+        count++;
+      }
+    });
+
+    return count;
+  } catch (error) {
+    console.log("Terjadi kesalahan");
+  }
+};
 
 module.exports = {
   promiseOutput,
